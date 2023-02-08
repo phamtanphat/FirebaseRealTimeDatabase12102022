0a15c6ff3bd49335273b9c5d3e32db0ee4e27a91
Phat <phatdroid94@gmail.com>
2023-02-08 19:19:38 +0700

    get value

--- app/src/main/java/com/example/firebaserealtimedatabase12102022/MainActivity.java
+++ app/src/main/java/com/example/firebaserealtimedatabase12102022/MainActivity.java
@@ -4,12 +4,16 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,11 +22,13 @@ public class MainActivity extends AppCompatActivity {
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference();
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tv = findViewById(R.id.text_view);
         // 1 : Gửi dữ liệu dạng chuỗi
 //        myRef.child("name").setValue("Phạm Tấn Phát").addOnCompleteListener(new OnCompleteListener<Void>() {
 //            @Override
@@ -54,14 +60,30 @@ public class MainActivity extends AppCompatActivity {
 //        });
 
         // 3: Gửi dạng object
-        myRef.child("animal").push().setValue(new Animal("Cat", 2.5)).addOnCompleteListener(new OnCompleteListener<Void>() {
+//        myRef.child("animal").push().setValue(new Animal("Cat", 2.5)).addOnCompleteListener(new OnCompleteListener<Void>() {
+//            @Override
+//            public void onComplete(@NonNull Task<Void> task) {
+//                if (task.isSuccessful()) {
+//                    Toast.makeText(MainActivity.this, "Thêm dữ liệu thành công", Toast.LENGTH_SHORT).show();
+//                } else {
+//                    Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+
+        // Lấy dữ liệu
+        // 1: Lấy dạng chuỗi
+
+        myRef.child("name").addValueEventListener(new ValueEventListener() {
             @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if (task.isSuccessful()) {
-                    Toast.makeText(MainActivity.this, "Thêm dữ liệu thành công", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                }
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String text = (String) snapshot.getValue();
+                tv.setText(text);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
